67b10753289d669f166a5ee8477a1aa9d554da7f
Emmanuel Thiessen <annoyingmoose.th@gmail.com>
2017-01-17 17:19:54 -0500

    Boilerplate
    Whoops, forgot #include boilerplating. Fixed.

--- mavi-pins.hpp
+++ mavi-pins.hpp
@@ -6,8 +6,13 @@
  * Pin #'s are WiringPi pin #'s. See http://wiringpi.com/pins for more details.
  */
 
+#ifndef MAVI_PINS_H
+#define MAVI_PINS_H
+
 #define MAVI_USL 0
 #define MAVI_USR 1
 #define MAVI_IRS 2
 #define MAVI_IRM 3
 #define MAVI_IRL 4
+
+#endif
