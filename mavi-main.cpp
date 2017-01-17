14e58f1b05b0ed8ef11bf884238aad566c90b911
Emmanuel Thiessen <annoyingmoose.th@gmail.com>
2017-01-17 09:27:58 -0500

    Entry/Main Loop Source
    Added a template source file for the MAVI software entry-point and main
    loop.

---
+++ mavi-main.cpp
@@ -0,0 +1,21 @@
+/*
+ *
+ */
+
+#include <ao/ao.h>
+#include <wiringPi.h>
+
+int main(int argc, char ** argv)
+{
+	// Initialization
+
+	// Main Loop
+	while (1)
+	{
+		// Poll sensors
+
+		// Process
+
+		// Send feedback
+	}
+}
