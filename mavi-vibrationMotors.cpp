9088d4ec587c6220759b80130a3120a906ffc468
Arangojd <arango.j.david@gmail.com>
2017-01-18 10:44:03 -0500

    Create mavi-vibrationMotors.cpp
    Vibration Motor Interfaces

---
+++ mavi-vibrationMotors.cpp
@@ -0,0 +1,22 @@
+/*
+ * mavi-vibrationMotors.cpp
+ * ----------------
+ *
+ * ...
+ */
+
+#include <wiringPi.h>
+#include "mavi-vibrationMotors.hpp"
+#include "mavi-pins.hpp"
+
+void maviVibrationMotorL(int vibrationOutputLength)
+{
+}
+
+void maviVibrationMotorR(int vibrationOutputLength)
+{
+}
+
+void maviVibrationMotorC(int vibrationOutputLength)
+{
+}
