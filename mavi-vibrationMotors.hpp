13e1995dbabcdb34f9569c5888688ddc80a60ffc
Arangojd <arango.j.david@gmail.com>
2017-01-18 10:39:28 -0500

    Create mavi-vibrationMotors.hpp

---
+++ mavi-vibrationMotors.hpp
@@ -0,0 +1,11 @@
+/*
+ * mavi-vibrationMotors.hpp
+ * ----------------
+ *
+ * Interfacing methods for vibration motors.
+ * When called, motor will vibrate for a number of milliseconds equal to vibrationOutputLength
+ */
+
+void maviVibrationMotorL(int vibrationOutputLength);
+void maviVibrationMotorL(int vibrationOutputLength);
+void maviVibrationMotorL(int vibrationOutputLength);
