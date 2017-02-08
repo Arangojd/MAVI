12e88821f6d138250d10184a33a1b4cbcbdbdc09
Arangojd <arango.j.david@gmail.com>
2017-02-08 11:50:51 -0500

    maviNextStepScan complete

--- mavi-analysis.cpp
+++ mavi-analysis.cpp
@@ -6,9 +6,24 @@
  */
 
 #include "mavi-analysis.hpp"
+#include "mavi-sensors.hpp"
+#include <cmath>
+using namespace std;
 
 MaviNextStepKind maviNextStepScan(void)
 {
+  irDist = maviPollSensor(MAVI_SENSOR_IRS);
+  irHeight = irDist * cos(refAngleIRS);
+  dif = refHeightBelt - irHeight;
+  
+  if (abs(dif) < 5)
+    return MAVI_NEXTSTEP_NOTHING;
+  else if ( dif > 15 && dif < 25)
+    return MAVI_NEXTSTEP_STEP_UP
+  else if (dif < -15 && dif > -25)
+    return MAVI_NEXTSTEP_STEP_DOWN
+  else 
+    return MAVI_NEXTSTEP_OBSTACLE
 }
 
 MaviSlopeKind maviSlopeScan(void)
