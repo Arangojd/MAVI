2c38cb81b4c4b9d3e16dbc6415d08bc72f392ee7
Emmanuel Thiessen <annoyingmoose.th@gmail.com>
2017-02-08 11:26:37 -0500

    Added analysis function skeletons

--- mavi-analysis.cpp
+++ mavi-analysis.cpp
@@ -7,6 +7,18 @@
 
 #include "mavi-analysis.hpp"
 
+MaviNextStepKind maviNextStepScan(void)
+{
+}
+
+MaviSlopeKind maviSlopeScan(void)
+{
+}
+
+MaviMidRangeKind maviMidRangeScan(void)
+{
+}
+
 void *maviSenseAndAnalyze(void* args)
 {
 }
--- mavi-analysis.hpp
+++ mavi-analysis.hpp
@@ -8,6 +8,35 @@
 #ifndef MAVI_ANALYSIS_HPP
 #define MAVI_ANALYSIS_HPP
 
+enum MaviNextStepKind
+{
+	MAVI_NEXTSTEP_NOTHING,
+	MAVI_NEXTSTEP_STAIRS,
+	MAVI_NEXTSTEP_OBSTACLE
+};
+
+enum MaviSlopeKind
+{
+	MAVI_SLOPE_FLAT,
+	MAVI_SLOPE_ASCENDING,
+	MAVI_SLOPE_DESCENDING,
+	MAVI_SLOPE_OTHER
+};
+
+enum MaviMidRangeKind
+{
+	MAVI_MIDRANGE_LEFT,
+	MAVI_MIDRANGE_RIGHT,
+	MAVI_MIDRANGE_BOTH,
+	MAVI_MIDRANGE_NEITHER
+};
+
+MaviNextStepKind maviNextStepScan(void);
+
+MaviSlopeKind maviSlopeScan(void);
+
+MaviMidRangeKind maviMidRangeScan(void);
+
 void *maviSenseAndAnalyze(void*);
 
 #endif
