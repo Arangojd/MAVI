77a5975a3665fb134d223e27696fd3a627de9f6a
Arangojd <arango.j.david@gmail.com>
2017-02-08 11:54:39 -0500

    Update mavi-analysis.cpp

--- mavi-analysis.cpp
+++ mavi-analysis.cpp
@@ -12,22 +12,26 @@ using namespace std;
 
 MaviNextStepKind maviNextStepScan(void)
 {
-  irDist = maviPollSensor(MAVI_SENSOR_IRS);
-  irHeight = irDist * cos(refAngleIRS);
-  dif = refHeightBelt - irHeight;
+    irDist = maviPollSensor(MAVI_SENSOR_IRS);
+    irHeight = irDist * cos(refAngleIRS);
+    dif = refHeightBelt - irHeight;
   
-  if (abs(dif) < 5)
-    return MAVI_NEXTSTEP_NOTHING;
-  else if ( dif > 15 && dif < 25)
-    return MAVI_NEXTSTEP_STEP_UP
-  else if (dif < -15 && dif > -25)
-    return MAVI_NEXTSTEP_STEP_DOWN
-  else 
-    return MAVI_NEXTSTEP_OBSTACLE
+    if (abs(dif) < 5)
+        return MAVI_NEXTSTEP_NOTHING;
+    else if ( dif > 15 && dif < 25)
+        return MAVI_NEXTSTEP_STEP_UP
+    else if (dif < -15 && dif > -25)
+        return MAVI_NEXTSTEP_STEP_DOWN
+    else 
+        return MAVI_NEXTSTEP_OBSTACLE
 }
 
 MaviSlopeKind maviSlopeScan(void)
 {
+    irMDist = maviPollSensor(MAVI_SENSOR_IRM);
+    irLDist = maviPollSensor(MAVI_SENSOR_IRL);
+  
+    irMHeight = 
 }
 
 MaviMidRangeKind maviMidRangeScan(void)
