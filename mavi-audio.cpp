52b39e68d10e156f79ba728ad93c1bb8493ccabb
Emmanuel Thiessen <annoyingmoose.th@gmail.com>
2017-02-27 12:22:57 -0500

    Syntactical corrections and reformatting for audio.(cpp|hpp)

--- mavi-audio.cpp
+++ mavi-audio.cpp
@@ -11,89 +11,89 @@
 
 static const int BUF_SIZE = 4096;
 
-struct WavHeader 
+struct WavHeader
 {
-    char id[4]; //should contain RIFF
-    int32_t totalLength;
-    char wavefmt[8];
-    int32_t format; // 16 for PCM
-    int16_t pcm; // 1 for PCM
-    int16_t channels;
-    int32_t frequency;
-    int32_t bytesPerSecond;
-    int16_t bytesByCapture;
-    int16_t bitsPerSample;
-    char data[4]; // "data"
-    int32_t bytesInData;
+	char id[4]; //should contain RIFF
+	int32_t totalLength;
+	char wavefmt[8];
+	int32_t format; // 16 for PCM
+	int16_t pcm; // 1 for PCM
+	int16_t channels;
+	int32_t frequency;
+	int32_t bytesPerSecond;
+	int16_t bytesByCapture;
+	int16_t bitsPerSample;
+	char data[4]; // "data"
+	int32_t bytesInData;
 };
 
-int maviAudioPlay(MaviAudioID audioFile) 
+int maviAudioPlay(MaviAudioID audioFile)
 {
 	/* Open file and process header */
 
-    	ao_device* device;
+	ao_device* device;
 	ao_sample_format format;
-    	int defaultDriver;
-    	WavHeader header;
+	int defaultDriver;
+	WavHeader header;
 
-    	std::ifstream file;
-    	file.open(audioFile, std::ios::binary | std::ios::in);
+	std::ifstream file;
+	file.open(audioFile, std::ios::binary | std::ios::in);
 
-    	file.read(header.id, sizeof(header.id));
-    	assert(!std::memcmp(header.id, "RIFF", 4)); //is it a WAV file?
-    	file.read((char*)&header.totalLength, sizeof(header.totalLength));
-    	file.read(header.wavefmt, sizeof(header.wavefmt)); //is it the right format?
-    	assert(!std::memcmp(header.wavefmt, "WAVEfmt ", 8));
-    	file.read((char*)&header.format, sizeof(header.format));
-    	file.read((char*)&header.pcm, sizeof(header.pcm));
-    	file.read((char*)&header.channels, sizeof(header.channels));
-    	file.read((char*)&header.frequency, sizeof(header.frequency));
-    	file.read((char*)&header.bytesPerSecond, sizeof(header.bytesPerSecond));
-    	file.read((char*)&header.bytesByCapture, sizeof(header.bytesByCapture));
-    	file.read((char*)&header.bitsPerSample, sizeof(header.bitsPerSample));
-    	file.read(header.data, sizeof(header.data));
-    	file.read((char*)&header.bytesInData, sizeof(header.bytesInData));
+	file.read(header.id, sizeof(header.id));
+	assert(!std::memcmp(header.id, "RIFF", 4)); //is it a WAV file?
+	file.read((char*)&header.totalLength, sizeof(header.totalLength));
+	file.read(header.wavefmt, sizeof(header.wavefmt)); //is it the right format?
+	assert(!std::memcmp(header.wavefmt, "WAVEfmt ", 8));
+	file.read((char*)&header.format, sizeof(header.format));
+	file.read((char*)&header.pcm, sizeof(header.pcm));
+	file.read((char*)&header.channels, sizeof(header.channels));
+	file.read((char*)&header.frequency, sizeof(header.frequency));
+	file.read((char*)&header.bytesPerSecond, sizeof(header.bytesPerSecond));
+	file.read((char*)&header.bytesByCapture, sizeof(header.bytesByCapture));
+	file.read((char*)&header.bitsPerSample, sizeof(header.bitsPerSample));
+	file.read(header.data, sizeof(header.data));
+	file.read((char*)&header.bytesInData, sizeof(header.bytesInData));
 
 	/* Initialize libao */
 
-    	ao_initialize();
+	ao_initialize();
 
-    	defaultDriver = ao_default_driver_id();
+	defaultDriver = ao_default_driver_id();
 
-    	memset(&format, 0, sizeof(format));
-    	format.bits = header.format;
-    	format.channels = header.channels;
-    	format.rate = header.frequency;
-    	format.byte_format = AO_FMT_LITTLE;
+	memset(&format, 0, sizeof(format));
+	format.bits = header.format;
+	format.channels = header.channels;
+	format.rate = header.frequency;
+	format.byte_format = AO_FMT_LITTLE;
 
-    	device = ao_open_live(defaultDriver, &format, NULL);
-    	if (device == NULL) 
+	device = ao_open_live(defaultDriver, &format, NULL);
+	if (device == NULL)
 	{
-        	cout << "Error: Unable to open audio driver." << endl << endl;
-        	return 1;
-    	}
-	
+		cout << "Error: Unable to open audio driver." << endl << endl;
+		return 1;
+	}
+
 	/* Load and play audio file  */
 
-    	char* buffer = (char*)malloc(BUF_SIZE * sizeof(char));
+	char* buffer = (char*)malloc(BUF_SIZE * sizeof(char));
 
-    	int fSize = header.bytesInData;
-    	int bCount = fSize / BUF_SIZE;
+	int fSize = header.bytesInData;
+	int bCount = fSize / BUF_SIZE;
 
-    	for (int i = 0; i < bCount; ++i) 
+	for (int i = 0; i < bCount; ++i)
 	{
-        	file.read(buffer, BUF_SIZE);
-        	ao_play(device, buffer, BUF_SIZE);
-    	}
+		file.read(buffer, BUF_SIZE);
+		ao_play(device, buffer, BUF_SIZE);
+	}
 
-    	int leftoverBytes = fSize % BUF_SIZE;
-    	file.read(buffer, leftoverBytes);
-    	memset(buffer + leftoverBytes, 0, BUF_SIZE - leftoverBytes);
-    	ao_play(device, buffer, BUF_SIZE);
-	
+	int leftoverBytes = fSize % BUF_SIZE;
+	file.read(buffer, leftoverBytes);
+	memset(buffer + leftoverBytes, 0, BUF_SIZE - leftoverBytes);
+	ao_play(device, buffer, BUF_SIZE);
+
 	/* Clean up and shutdown */
 
-    	ao_close(device);
-    	ao_shutdown();
+	ao_close(device);
+	ao_shutdown();
 	file.close();
 }
--- mavi-audio.hpp
+++ mavi-audio.hpp
@@ -1,34 +1,35 @@
 #ifndef MAVI_AUDIO_HPP
 #define MAVI_AUDIO_HPP
 
-typedef MaviAudioID char*;
+typedef char* MaviAudioID;
 
-MaviAudioID MAVI_AUDIO_BTN_CALIB = "audio/mavi-button-calibration.wav";
-MaviAudioID MAVI_AUDIO_BTN_POWER = "audio/mavi-button-power.wav";
-MaviAudioID MAVI_AUDIO_BTN_SLEEP = "audio/mavi-button-sleep.wav";
-MaviAudioID MAVI_AUDIO_CALIB_FAILED = "audio/mavi-calibration-failed.wav"
-MaviAudioID MAVI_AUDIO_CALIB_STARTED = "audio/mavi-calibration-started.wav"
-MaviAudioID MAVI_AUDIO_CALIB_SUCCESS = "audio/mavi-calibration-sucessful.wav"
-MaviAudioID MAVI_AUDIO_IM_HAZARD = "audio/mavi-immediate-hazard.wav"
-MaviAudioID MAVI_AUDIO_STAIRS_ASC = "audio/mavi-stairs-ascending.wav"
-MaviAudioID MAVI_AUDIO_STAIRS_DESC = "audio/mavi-stairs-descending.wav"
-MaviAudioID MAVI_AUDIO_STEP_FINAL = "audio/mavi-step-final.wav"
-MaviAudioID MAVI_AUDIO_STEP_FIRSTDOWN = "audio/mavi-step-first-down.wav"
-MaviAudioID MAVI_AUDIO_STEP_FIRSTUP = "audio/mavi-step-first-up.wav"
-MaviAudioID MAVI_AUDIO_STEP_SINGLEDOWN = "audio/mavi-step-single-down.wav"
-MaviAudioID MAVI_AUDIO_STEP_SINGLEUP = "audio/mavi-step-single-up.wav"
-MaviAudioID MAVI_AUDIO_SYSTEM_ON = "audio/mavi-system-on.wav"
-MaviAudioID MAVI_AUDIO_SYSTEM_PAUSED = "audio/mavi-system-paused.wav"
-MaviAudioID MAVI_AUDIO_SYSTEM_READY = "audio/mavi-system-ready.wav"
-MaviAudioID MAVI_AUDIO_SYSTEM_SHUTDOWN = "audio/mavi-system-shutdown.wav"
-MaviAudioID MAVI_AUDIO_VIBRATE_CENTER = "audio/mavi-vibrate-center.wav"
-MaviAudioID MAVI_AUDIO_VIBRATE_LEFT = "audio/mavi-vibrate-left.wav"
-MaviAudioID MAVI_AUDIO_VIBRATE_RIGHT = "audio/mavi-vibrate-right.wav"
-MaviAudioID MAVI_AUDIO_WARNING_BATTERYLOW = "audio/mavi-warning-battery-low.wav"
-MaviAudioID MAVI_AUDIO_WARNING_BATTERYCRITICAL = "audio/mavi-warning-battery-critical.wav"
-MaviAudioID MAVI_AUDIO_WARNING_NONOPERATIONAL = "audio/mavi-warning-nonoperational.wav"
-MaviAudioID MAVI_AUDIO_WARNING_SENSORFAILURE = "audio/mavi-warning-sensor-failure.wav"
+MaviAudioID
+	MAVI_AUDIO_BTN_CALIB = "audio/mavi-button-calibration.wav",
+	MAVI_AUDIO_BTN_POWER = "audio/mavi-button-power.wav",
+	MAVI_AUDIO_BTN_SLEEP = "audio/mavi-button-sleep.wav",
+	MAVI_AUDIO_CALIB_FAILED = "audio/mavi-calibration-failed.wav",
+	MAVI_AUDIO_CALIB_STARTED = "audio/mavi-calibration-started.wav",
+	MAVI_AUDIO_CALIB_SUCCESS = "audio/mavi-calibration-sucessful.wav",
+	MAVI_AUDIO_IM_HAZARD = "audio/mavi-immediate-hazard.wav",
+	MAVI_AUDIO_STAIRS_ASC = "audio/mavi-stairs-ascending.wav",
+	MAVI_AUDIO_STAIRS_DESC = "audio/mavi-stairs-descending.wav",
+	MAVI_AUDIO_STEP_FINAL = "audio/mavi-step-final.wav",
+	MAVI_AUDIO_STEP_FIRSTDOWN = "audio/mavi-step-first-down.wav",
+	MAVI_AUDIO_STEP_FIRSTUP = "audio/mavi-step-first-up.wav",
+	MAVI_AUDIO_STEP_SINGLEDOWN = "audio/mavi-step-single-down.wav",
+	MAVI_AUDIO_STEP_SINGLEUP = "audio/mavi-step-single-up.wav",
+	MAVI_AUDIO_SYSTEM_ON = "audio/mavi-system-on.wav",
+	MAVI_AUDIO_SYSTEM_PAUSED = "audio/mavi-system-paused.wav",
+	MAVI_AUDIO_SYSTEM_READY = "audio/mavi-system-ready.wav",
+	MAVI_AUDIO_SYSTEM_SHUTDOWN = "audio/mavi-system-shutdown.wav",
+	MAVI_AUDIO_VIBRATE_CENTER = "audio/mavi-vibrate-center.wav",
+	MAVI_AUDIO_VIBRATE_LEFT = "audio/mavi-vibrate-left.wav",
+	MAVI_AUDIO_VIBRATE_RIGHT = "audio/mavi-vibrate-right.wav",
+	MAVI_AUDIO_WARNING_BATTERYLOW = "audio/mavi-warning-battery-low.wav",
+	MAVI_AUDIO_WARNING_BATTERYCRITICAL = "audio/mavi-warning-battery-critical.wav",
+	MAVI_AUDIO_WARNING_NONOPERATIONAL = "audio/mavi-warning-nonoperational.wav",
+	MAVI_AUDIO_WARNING_SENSORFAILURE = "audio/mavi-warning-sensor-failure.wav";
 
-int maviAudiPlay(MaviAudioID audioFile);
+int maviAudioPlay(MaviAudioID audioFile);
 
 #endif
