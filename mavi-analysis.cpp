69ecb3ec65b1072a560559abd466e96f011f237a
Arangojd <arango.j.david@gmail.com>
2017-03-03 11:16:15 -0500

    Update mavi-analysis.cpp

--- mavi-analysis.cpp
+++ mavi-analysis.cpp
@@ -153,10 +153,12 @@ void *maviSenseAndAnalyze(void* args)
 			switch (slopeScan)
 			{
 			case MAVI_SLOPE_ASCENDING:
+				maviAudioPlay(MAVI_AUDIO_STAIRS_ASC);
 				cout << "Verbal Output: Ascending Stairs Ahead";
 				break;
 
 			case MAVI_SLOPE_DESCENDING:
+				maviAudioPlay(MAVI_AUDIO_STAIRS_DESC);
 				cout << "Verbal Output: Descending Stairs Ahead";
 				break;
 
@@ -164,14 +166,17 @@ void *maviSenseAndAnalyze(void* args)
 				switch (maviMidRangeScan())
 				{
 				case MAVI_MIDRANGE_BOTH:
-					cout << "Vibration Output: Center (Obstacle Ahead)";
+					//maviAudioPlay(MAVI_AUDIO_VIBRATE_BOTH);
+					cout << "Vibration Output: Both (Obstacle Left & Right)";
 					break;
 
 				case MAVI_MIDRANGE_LEFT:
+					maviAudioPlay(MAVI_AUDIO_VIBRATE_LEFT);
 					cout << "Vibration Output: Left (Obstacle on Left)";
 					break;
 
 				case MAVI_MIDRANGE_RIGHT:
+					maviAudioPlay(MAVI_AUDIO_VIBRATE_RIGHT);
 					cout << "Vibration Output: Right (Obstacle on Right)";
 					break;
 
@@ -187,6 +192,7 @@ void *maviSenseAndAnalyze(void* args)
 				break;
 
 			case MAVI_SLOPE_OTHER:
+				maviAudioPlay(MAVI_AUDIO_VIBRATE_CENTER);
 				cout << "Vibration Output: Center (Obstacle Ahead)";
 				break;
 
@@ -201,18 +207,25 @@ void *maviSenseAndAnalyze(void* args)
 			switch (slopeScan)
 			{
 			case MAVI_SLOPE_ASCENDING:
+				maviAudioPlay(MAVI_AUDIO_STEP_FIRSTUP);
 				cout << "Verbal Output: (First) Step Up";
 				break;
 
 			case MAVI_SLOPE_FLAT:
+				maviAudioPlay(MAVI_AUDIO_STEP_SINGLEUP);
 				cout << "Verbal Output: Single/Last Step Up";
 				break;
 
 			case MAVI_SLOPE_DESCENDING:
-			case MAVI_SLOPE_OTHER:
+				maviAudioPlay(MAVI_AUDIO_IM_HAZARD);
 				cout << "Omni-Output: Immediate Hazard Warning";
 				break;
-
+					
+			case MAVI_SLOPE_OTHER:
+				maviAudioPlay(MAVI_AUDIO_IM_HAZARD);
+				cout << "Omni-Output: Immediate Hazard Warning";
+				break;
+					
 			default:
 				cout << "Sensing and Analysis Error: Received invalid slope scan data.";
 				break;
@@ -224,21 +237,25 @@ void *maviSenseAndAnalyze(void* args)
 			switch (slopeScan)
 			{
 			case MAVI_SLOPE_DESCENDING:
-
+				maviAudioPlay(MAVI_AUDIO_STEP_FIRSTDOWN);
 				cout << "Verbal Output: (First) Step Down";
 				break;
 
 			case MAVI_SLOPE_FLAT:
-
+				maviAudioPlay(MAVI_AUDIO_STEP_FIRSTDOWN);
 				cout << "Verbal Output: Single/Last Step Down";
 				break;
-
+					
 			case MAVI_SLOPE_ASCENDING:
-			case MAVI_SLOPE_OTHER:
-
+				maviAudioPlay(MAVI_AUDIO_IM_HAZARD);
 				cout << "Omni-Output: Immediate Hazard Warning";
 				break;
-
+					
+			case MAVI_SLOPE_OTHER:
+				maviAudioPlay(MAVI_AUDIO_IM_HAZARD);
+				cout << "Omni-Output: Immediate Hazard Warning";
+				break;
+					
 			default:
 				cout << "Sensing and Analysis Error: Received invalid slope scan data.";
 				break;
@@ -247,6 +264,7 @@ void *maviSenseAndAnalyze(void* args)
 			break;
 
 		case MAVI_NEXTSTEP_OBSTACLE:
+			maviAudioPlay(MAVI_AUDIO_IM_HAZARD);
 			cout << "Omni-Output: Immediate Hazard Warning";
 			break;
 
