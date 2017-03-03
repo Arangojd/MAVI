c27ccbf583018e15116b374dd45d6fc1b91041bd
Emmanuel Thiessen <annoyingmoose.th@gmail.com>
2017-03-03 11:04:28 -0500

    Moved audio feedback filename definitions to audio.cpp

--- mavi-audio.cpp
+++ mavi-audio.cpp
@@ -9,6 +9,33 @@
 #include <cstring>
 #include "mavi-audio.hpp"
 
+const MaviAudioID
+	MAVI_AUDIO_BTN_CALIB = "audio/mavi-button-calibration.wav",
+	MAVI_AUDIO_BTN_POWER = "audio/mavi-button-power.wav",
+	MAVI_AUDIO_BTN_SLEEP = "audio/mavi-button-sleep.wav",
+	MAVI_AUDIO_CALIB_FAILED = "audio/mavi-calibration-failed.wav",
+	MAVI_AUDIO_CALIB_STARTED = "audio/mavi-calibration-started.wav",
+	MAVI_AUDIO_CALIB_SUCCESS = "audio/mavi-calibration-sucessful.wav",
+	MAVI_AUDIO_IM_HAZARD = "audio/mavi-immediate-hazard.wav",
+	MAVI_AUDIO_STAIRS_ASC = "audio/mavi-stairs-ascending.wav",
+	MAVI_AUDIO_STAIRS_DESC = "audio/mavi-stairs-descending.wav",
+	MAVI_AUDIO_STEP_FINAL = "audio/mavi-step-final.wav",
+	MAVI_AUDIO_STEP_FIRSTDOWN = "audio/mavi-step-first-down.wav",
+	MAVI_AUDIO_STEP_FIRSTUP = "audio/mavi-step-first-up.wav",
+	MAVI_AUDIO_STEP_SINGLEDOWN = "audio/mavi-step-single-down.wav",
+	MAVI_AUDIO_STEP_SINGLEUP = "audio/mavi-step-single-up.wav",
+	MAVI_AUDIO_SYSTEM_ON = "audio/mavi-system-on.wav",
+	MAVI_AUDIO_SYSTEM_PAUSED = "audio/mavi-system-paused.wav",
+	MAVI_AUDIO_SYSTEM_READY = "audio/mavi-system-ready.wav",
+	MAVI_AUDIO_SYSTEM_SHUTDOWN = "audio/mavi-system-shutdown.wav",
+	MAVI_AUDIO_VIBRATE_CENTER = "audio/mavi-vibrate-center.wav",
+	MAVI_AUDIO_VIBRATE_LEFT = "audio/mavi-vibrate-left.wav",
+	MAVI_AUDIO_VIBRATE_RIGHT = "audio/mavi-vibrate-right.wav",
+	MAVI_AUDIO_WARNING_BATTERYLOW = "audio/mavi-warning-battery-low.wav",
+	MAVI_AUDIO_WARNING_BATTERYCRITICAL = "audio/mavi-warning-battery-critical.wav",
+	MAVI_AUDIO_WARNING_NONOPERATIONAL = "audio/mavi-warning-nonoperational.wav",
+	MAVI_AUDIO_WARNING_SENSORFAILURE = "audio/mavi-warning-sensor-failure.wav";
+
 int maviAudioPlay(MaviAudioID audioFile)
 {
 	char cmd[strlen(audioFile) + 10];
--- mavi-audio.hpp
+++ mavi-audio.hpp
@@ -10,32 +10,32 @@
 
 typedef const char* MaviAudioID;
 
-const MaviAudioID
-	MAVI_AUDIO_BTN_CALIB = "audio/mavi-button-calibration.wav",
-	MAVI_AUDIO_BTN_POWER = "audio/mavi-button-power.wav",
-	MAVI_AUDIO_BTN_SLEEP = "audio/mavi-button-sleep.wav",
-	MAVI_AUDIO_CALIB_FAILED = "audio/mavi-calibration-failed.wav",
-	MAVI_AUDIO_CALIB_STARTED = "audio/mavi-calibration-started.wav",
-	MAVI_AUDIO_CALIB_SUCCESS = "audio/mavi-calibration-sucessful.wav",
-	MAVI_AUDIO_IM_HAZARD = "audio/mavi-immediate-hazard.wav",
-	MAVI_AUDIO_STAIRS_ASC = "audio/mavi-stairs-ascending.wav",
-	MAVI_AUDIO_STAIRS_DESC = "audio/mavi-stairs-descending.wav",
-	MAVI_AUDIO_STEP_FINAL = "audio/mavi-step-final.wav",
-	MAVI_AUDIO_STEP_FIRSTDOWN = "audio/mavi-step-first-down.wav",
-	MAVI_AUDIO_STEP_FIRSTUP = "audio/mavi-step-first-up.wav",
-	MAVI_AUDIO_STEP_SINGLEDOWN = "audio/mavi-step-single-down.wav",
-	MAVI_AUDIO_STEP_SINGLEUP = "audio/mavi-step-single-up.wav",
-	MAVI_AUDIO_SYSTEM_ON = "audio/mavi-system-on.wav",
-	MAVI_AUDIO_SYSTEM_PAUSED = "audio/mavi-system-paused.wav",
-	MAVI_AUDIO_SYSTEM_READY = "audio/mavi-system-ready.wav",
-	MAVI_AUDIO_SYSTEM_SHUTDOWN = "audio/mavi-system-shutdown.wav",
-	MAVI_AUDIO_VIBRATE_CENTER = "audio/mavi-vibrate-center.wav",
-	MAVI_AUDIO_VIBRATE_LEFT = "audio/mavi-vibrate-left.wav",
-	MAVI_AUDIO_VIBRATE_RIGHT = "audio/mavi-vibrate-right.wav",
-	MAVI_AUDIO_WARNING_BATTERYLOW = "audio/mavi-warning-battery-low.wav",
-	MAVI_AUDIO_WARNING_BATTERYCRITICAL = "audio/mavi-warning-battery-critical.wav",
-	MAVI_AUDIO_WARNING_NONOPERATIONAL = "audio/mavi-warning-nonoperational.wav",
-	MAVI_AUDIO_WARNING_SENSORFAILURE = "audio/mavi-warning-sensor-failure.wav";
+extern const MaviAudioID
+	MAVI_AUDIO_BTN_CALIB,
+	MAVI_AUDIO_BTN_POWER,
+	MAVI_AUDIO_BTN_SLEEP,
+	MAVI_AUDIO_CALIB_FAILED,
+	MAVI_AUDIO_CALIB_STARTED,
+	MAVI_AUDIO_CALIB_SUCCESS,
+	MAVI_AUDIO_IM_HAZARD,
+	MAVI_AUDIO_STAIRS_ASC,
+	MAVI_AUDIO_STAIRS_DESC,
+	MAVI_AUDIO_STEP_FINAL,
+	MAVI_AUDIO_STEP_FIRSTDOWN,
+	MAVI_AUDIO_STEP_FIRSTUP,
+	MAVI_AUDIO_STEP_SINGLEDOWN,
+	MAVI_AUDIO_STEP_SINGLEUP,
+	MAVI_AUDIO_SYSTEM_ON,
+	MAVI_AUDIO_SYSTEM_PAUSED,
+	MAVI_AUDIO_SYSTEM_READY,
+	MAVI_AUDIO_SYSTEM_SHUTDOWN,
+	MAVI_AUDIO_VIBRATE_CENTER,
+	MAVI_AUDIO_VIBRATE_LEFT,
+	MAVI_AUDIO_VIBRATE_RIGHT,
+	MAVI_AUDIO_WARNING_BATTERYLOW,
+	MAVI_AUDIO_WARNING_BATTERYCRITICAL,
+	MAVI_AUDIO_WARNING_NONOPERATIONAL,
+	MAVI_AUDIO_WARNING_SENSORFAILURE;
 
 int maviAudioPlay(MaviAudioID audioFile);
 
