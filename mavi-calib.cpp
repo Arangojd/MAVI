0e31eb5844f9a880c1a7e24b2ad824ab31cc9fff
Arangojd <arango.j.david@gmail.com>
2017-03-09 13:28:23 -0500

    Update mavi-calib.cpp

--- mavi-calib.cpp
+++ mavi-calib.cpp
@@ -5,7 +5,13 @@
  * ...
  */
 
+#inlcude <cmath>
+#include <iostream>
+
+#include "mavi-sensors.hpp"
+#include "mavi-audio.hpp"
 #include "mavi-calib.hpp"
+#include "mavi-math.hpp"
 
 double
 	refDistUSL = MAVI_DEFAULT_REF_DIST_USL,
@@ -15,3 +21,54 @@ double
 	refDistIRL = MAVI_DEFAULT_REF_DIST_IRL,
 	refSlope = MAVI_DEFAULT_REF_SLOPE;
 	
+int maviCalibration(void)
+{
+	cout << "Calibration Started" << endl;
+	maviAudioPlay(MAVI_AUDIO_CALIB_STARTED);
+	
+	irSDist = maviIRSFilter.poll();
+	if (abs(refDistIRS - irSDist) > MAVI_ERROR_MARGIN) {
+		cout << "Calibration Failed" << endl << endl;
+		maviAudioPlay(MAVI_AUDIO_CALIB_FAILED);
+		return 1;
+	}
+	
+	irMDist = maviIRMFilter.poll();
+	if (abs(refDistIRM - irMDist) > MAVI_ERROR_MARGIN) {
+		cout << "Calibration Failed" << endl << endl;
+		maviAudioPlay(MAVI_AUDIO_CALIB_FAILED);
+		return 1;
+	}
+	
+	irLDist = maviIRLFilter.poll();
+	if (abs(refDistIRL - irLDist) > MAVI_ERROR_MARGIN) {
+		cout << "Calibration Failed" << endl << endl;
+		maviAudioPlay(MAVI_AUDIO_CALIB_FAILED);
+		return 1;
+	}
+	
+	usLDist = maviUSLFilter.poll();
+	if (abs(refDistUSL - usLDist) > MAVI_ERROR_MARGIN) {
+		cout << "Calibration Failed" << endl << endl;
+		maviAudioPlay(MAVI_AUDIO_CALIB_FAILED);
+		return 1;
+	}
+
+	usRDist = maviUSRFilter.poll();
+	if (abs(refDistUSR - usRDist) > MAVI_ERROR_MARGIN) {
+		cout << "Calibration Failed" << endl << endl;
+		maviAudioPlay(MAVI_AUDIO_CALIB_FAILED);
+		return 1;
+	}
+	
+	refDistIRS = irSDist;	
+	refDistIRM = irMDist;
+	refDistIRL = irLDist;
+	refDistUSL = usLDist;
+	refDistUSR = usRDist;
+	refSlope = maviGetRefSlope(refDistIRS, refDistIRM, refDistIRL);
+	
+	cout << "Calibration Sucessful" << endl << endl;
+	maviAudioPlay(MAVI_AUDIO_CALIB_SUCCESS);
+	return 0;
+}
