b707847e556c00a136b34cc4e3b3f1a36acc6647
Arangojd <arango.j.david@gmail.com>
2017-03-09 13:06:53 -0500

    Create mavi-math.hpp

---
+++ mavi-math.hpp
@@ -0,0 +1,8 @@
+#ifndef MAVI_MATH_HPP
+#define MAVI_MATH_HPP
+
+double maviGetSlope(double s_measured, double m_measured, double l_measured);
+
+double maviGetRefSlope(double s_measured, double m_measured, double l_measured);
+
+#endif
