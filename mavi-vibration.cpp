0a7f0de60d879846eda105831c5e6e51cfc23082
Emmanuel Thiessen <annoyingmoose.th@gmail.com>
2017-03-21 11:39:34 -0400

    maviVibrate(...) no longer takes a force value

--- mavi-vibration.cpp
+++ mavi-vibration.cpp
@@ -12,21 +12,11 @@
 #include "mavi-pins.hpp"
 
 const MaviDigitalPin pins[3] = {MAVI_DPIN_VR, MAVI_DPIN_VC, MAVI_DPIN_VL};
-//unsigned int halfperiods[3];
 unsigned int durations[3];
 
 void *vibrateFunc(void *v)
 {
 	int i = (int)v;
-	//unsigned int end_t = millis() + durations[i];
-
-//	while (millis() < end_t)
-//	{
-//		digitalWrite(pins[i], 1);
-//		delay(halfperiods[i]);
-//		digitalWrite(pins[i], 0);
-//		delay(halfperiods[i]);
-//	}
 
 	digitalWrite(pins[i], 1);
 	delay(durations[i]);
@@ -35,18 +25,15 @@ void *vibrateFunc(void *v)
 	return NULL;
 }
 
-void maviVibrate(MaviVibratorID vibrators, double force, unsigned int duration)
+void maviVibrate(MaviVibratorID vibrators, unsigned int duration)
 {
 	static pthread_t vibThreads[3];
-
-	//unsigned int hp = (unsigned int)(1.0 / force); // Numerator may need to be larger
 
 	for (int i = 0; i < 3; i++)
 	{
 		if (vibrators & (1 << i))
 		{
 			pthread_cancel(vibThreads[i]);
-			//halfperiods[i] = hp;
 			durations[i] = duration;
 			pthread_create(&vibThreads[i], NULL, vibrateFunc, (void*)i);
 		}
--- mavi-vibration.hpp
+++ mavi-vibration.hpp
@@ -2,8 +2,8 @@
  * mavi-vibration.hpp
  * ------------------
  *
- * This header file contains the interface for the vibration function.
- * When called, the appropriate motor will vibrate for the specified duration and with the specified intensity.
+ * This header file contains the interface maviVibrate(MaviVibratorID, unsigned int).
+ * When called, the appropriate motors will vibrate for the specified duration.
  */
 
 #ifndef MAVI_VIBRATION_HPP
@@ -16,6 +16,6 @@ const MaviVibratorID
 	MAVI_VIB_C = 0b010,
 	MAVI_VIB_R = 0b001;
 
-void maviVibrate(MaviVibratorID vibrators, double force, unsigned int duration);
+void maviVibrate(MaviVibratorID vibrators, unsigned int duration);
 
 #endif
