99c995055e0a0813ebc4741dd5172e7761969325
Raspberry Pi <arango.j.david@gmail.com>
2017-04-06 12:15:36 -0400

    asdf

--- mavi-math.cpp
+++ mavi-math.cpp
@@ -7,12 +7,12 @@ double maviGetSlope(double s_measured, double m_measured, double l_measured)
 {
 	double m_height, l_height, m_length, l_length, net_height, net_length, measured_slope, actual_slope;
 
-	m_height = s_measured - (m_measured * cos(MAVI_REF_ANGLE_IRM));
-	l_height = s_measured - (l_measured * cos(MAVI_REF_ANGLE_IRL));
-	net_height = l_height - m_height;
+	m_height = (m_measured * cos(3.14/2 - MAVI_REF_ANGLE_IRM));
+	l_height = (l_measured * cos(3.14/2 - MAVI_REF_ANGLE_IRL));
+	net_height = m_height - l_height;
 
-	m_length = m_measured * sin(MAVI_REF_ANGLE_IRM);
-	l_length = l_measured * sin(MAVI_REF_ANGLE_IRL);
+	m_length = m_measured * sin(3.14/2 - MAVI_REF_ANGLE_IRM);
+	l_length = l_measured * sin(3.14/2 - MAVI_REF_ANGLE_IRL);
 	net_length = l_length - m_length;
 
 	measured_slope = net_height/net_length;
@@ -25,12 +25,12 @@ double maviGetRefSlope(double s_measured, double m_measured, double l_measured)
 {
 	double m_height, l_height, m_length, l_length, net_height, net_length, ref_slope;
 
-	m_height = s_measured - (m_measured * cos(MAVI_REF_ANGLE_IRM));
-	l_height = s_measured - (l_measured * cos(MAVI_REF_ANGLE_IRL));
-	net_height = l_height - m_height;
+	m_height = (m_measured * cos(3.14/2 - MAVI_REF_ANGLE_IRM));
+	l_height = (l_measured * cos(3.14/2 - MAVI_REF_ANGLE_IRL));
+	net_height = m_height - l_height;
 
-	m_length = m_measured * sin(MAVI_REF_ANGLE_IRM);
-	l_length = l_measured * sin(MAVI_REF_ANGLE_IRL);
+	m_length = m_measured * sin(3.14/2 - MAVI_REF_ANGLE_IRM);
+	l_length = l_measured * sin(3.14/2 - MAVI_REF_ANGLE_IRL);
 	net_length = l_length - m_length;
 
 	ref_slope = net_height/net_length;
